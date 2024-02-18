f749ff108f4cfb2c0c71306c9b78f14ecf72ef6c
mayankch283 <mayankch283@gmail.com>
2024-02-18 19:25:24 +0530

    priority scheduling algo

--- priority.c
+++ priority.c
@@ -1,36 +1,43 @@
-#include<stdio.h>
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 typedef struct
 {
-int pno; int pri; int pri; int btime;
-int wtime;
-}sp;
+    int pno;
+    int pri;
+    int pri;
+    int btime;
+    int wtime;
+} sp;
 int main()
 {
-int i,j,n;
-int tbm=0,totwtime=0,totttime=0; sp *p,t;
-printf("\n PRIORITY SCHEDULING.\n");
-printf("\n enter the no of process.
- \n");
+    int i, j, n;
+    int tbm = 0, totwtime = 0, totttime = 0;
+    sp *p, t;
+    printf("\n PRIORITY SCHEDULING.\n");
+printf("\n enter the no of process.\n");
 scanf("%d",&n); p=(sp*)malloc(sizeof(sp));
 printf("enter the burst time and priority:\n"); for(i=0;i<n;i++)
 {
-printf("process%d:”,i+1); scanf("%d%d",&p[i].btime,&p[i].pri);
+printf("process%d:",i+1); scanf("%d%d",&p[i].btime,&p[i].pri);
 p[i].pno=i+1;
 p[i].wtime=0;
 }
 for(i=0;i<n-1;i++) for(j=i+1;j<n;j++)
 {
-if(p[i].pri>p[j].pri)
-{
-t=p[i]; p[i]=p[j]; p[j]=t;
-}
+        if (p[i].pri > p[j].pri)
+        {
+            t = p[i];
+            p[i] = p[j];
+            p[j] = t;
+        }
 }
 printf("\n process\tbursttime\twaiting time\tturnaround time\n"); for(i=0;i<n;i++)
 {
-totwtime+=p[i].wtime=tbm; tbm+=p[i].btime; printf("\n%d\t\t%d",p[i].pno,p[i].btime);
-printf("\t\t%d\t\t%d",p[i].wtime,p[i].wtime+p[i].btime);
+        totwtime += p[i].wtime = tbm;
+        tbm += p[i].btime;
+        printf("\n%d\t\t%d", p[i].pno, p[i].btime);
+        printf("\t\t%d\t\t%d", p[i].wtime, p[i].wtime + p[i].btime);
 }
 totttime=tbm+totwtime;
 printf("\n total waiting time:%d",totwtime);
